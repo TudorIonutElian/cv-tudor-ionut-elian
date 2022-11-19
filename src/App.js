a3ef936d8a5f3a615b47d2050b849c0b7844da92
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-11-19 21:11:02 +0200

    Import all components into APP component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Profile />
+      <Experience />
+      <Education />
     </div>
   );
 }
